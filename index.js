e855f2ce380a63bc75f32830580752a8e666c41f
Lou Fletcher <lfletcher08@gmail.com>
2020-07-29 20:53:14 -0400

    started on .env

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const PORT = 8080;
 const express = require("express");
 const server = express();
@@ -13,6 +15,35 @@ server.use("/api", apiRouter);
 
 const { client } = require("./db");
 client.connect();
+
+server.use(async (req, res, next) => {
+  const prefix = "Bearer ";
+  const auth = req.headers["Authorization"];
+
+  if (!auth) {
+    next(); // don't set req.user, no token was passed in
+  }
+
+  if (auth.startsWith(prefix)) {
+    // recover the token
+    const token = auth.slice(prefix.length);
+    try {
+      // recover the data
+      const { id } = jwt.verify(data, "secret message");
+
+      // get the user from the database
+      const user = await getUserById(id);
+      // note: this might be a user or it might be null depending on if it exists
+
+      // attach the user and move on
+      req.user = user;
+
+      next();
+    } catch (error) {
+      // there are a few types of errors here
+    }
+  }
+});
 
 server.listen(PORT, () => {
   console.log("The server is up on port", PORT);
